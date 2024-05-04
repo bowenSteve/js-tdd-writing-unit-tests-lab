a455f5d3909b4320b64eab3b59dc193a3ee7b3b2
Stephen Bowen <bowensteven17@gmail.com>
2024-05-04 09:12:40 +0300

    js tdd writing unit tests lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,35 +1,22 @@
 import { isPalindrome } from "../utils";
 
 describe("isPalindrome", () => {
-  it("returns true when the word is the same forward and backward", () => {
-    const word = "racecar";
+  const testCases = [
+    { word: "racecar", expected: true },
+    { word: "car", expected: false },
+    { word: "RaCecar", expected: true },
+    { word: "race car", expected: null, errorExpected: true },
+    { word: "", expected: null, errorExpected: true }
+  ];
 
-    const result = isPalindrome(word);
-
-    expect(result).toBe(true);
-  });
-  it("returns false when the word is not the same forward and backward", () => {
-    const word = "car";
-
-    const result = isPalindrome(word);
-
-    expect(result).toBe(false);
-  });
-  it("return true for words that are a combination of uppercase and lowercase letters", () => {
-    const word = "RaCecar";
-
-    const result = isPalindrome(word);
-
-    expect(result).toBe(true);
-  });
-  it("throws an error if the input has any non-alphabetic characters", () => {
-    const word = "race car";
-
-    expect(() => isPalindrome(word)).toThrow();
-  });
-  it("throws an error if the input is an empty string", () => {
-    const word = "";
-
-    expect(() => isPalindrome(word)).toThrow();
+  testCases.forEach(({ word, expected, errorExpected }) => {
+    it(`returns ${expected} for "${word}"`, () => {
+      if (errorExpected) {
+        expect(() => isPalindrome(word)).toThrow();
+      } else {
+        const result = isPalindrome(word);
+        expect(result).toBe(expected);
+      }
+    });
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,12 @@
-function reverse(string) {
+function reverseString(string) {
     return string.split("").reverse().join("");
   }
   
   export function isPalindrome(word) {
     if (!/^[A-Za-z]+$/.test(word)) {
-      throw new Error("Invalid input");
+      throw new Error("Input must contain only alphabetic characters");
     }
     const lowerCaseWord = word.toLowerCase();
-    return lowerCaseWord === reverse(lowerCaseWord);
+    return lowerCaseWord === reverseString(lowerCaseWord);
   }
+  
